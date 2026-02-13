2c1f73ae6c10876a567b6e6538d2f3cc63240cd6
Hardik Sahni <kritikagautam875@gmail.com>
2026-02-13 10:51:31 +0530

    UseCase1

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,17 @@
+public class PalindromeCheckerApp {
+    /*
+    * UC1 -- Application entry point
+
+    * This is the first method runed by JVM
+    * when the program starts
+
+    *@author Developer
+    * @Version 1.0
+
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System Initialized " + "Successfully");
+    }
+}
